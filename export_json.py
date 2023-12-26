3f8f2de32911ac298421df334f69309416b41415
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 23:08:33 +0200

    Update export_json.py

--- export_json.py
+++ export_json.py
@@ -1,40 +1,32 @@
-import json
+import csv
 import psycopg2
-from psycopg2.extras import RealDictCursor
 
 try:
     # Підключення до PostgreSQL бази даних
-    conn = psycopg2.connect(
+    with psycopg2.connect(
         user='postgres',
         password='postgres',
-        dbname='db_lab3',
+        dbname='lab_3_6',
         host='localhost',
         port='5432'
-    )
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    ) as conn:
+        with conn.cursor() as cursor:
+            # Функція для експорту даних з таблиці у CSV-файл
+            def export_to_csv(table_name, csv_filename):
+                query = f"COPY (SELECT * FROM {table_name}) TO STDOUT WITH CSV HEADER"
+                try:
+                    with open(csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
+                        cursor.copy_expert(sql=query, file=csv_file)
+                    print(f'Data from {table_name} exported to {csv_filename}.')
+                except psycopg2.Error as e:
+                    print(f"Error exporting data from {table_name} to {csv_filename}: {e}")
 
-    # Функція для експорту даних з таблиці у JSON-файл
-    def export_to_json(table_name, json_filename):
-        query = f"SELECT * FROM {table_name};"
-        cursor.execute(query)
-        rows = cursor.fetchall()
-
-        with open(json_filename, 'w', encoding='utf-8') as json_file:
-            json.dump(rows, json_file, ensure_ascii=False, indent=2)
-
-        print(f'Data from {table_name} exported to {json_filename}.')
-
-    # Експорт даних з кожної таблиці
-    export_to_json('MoviesAndShows', 'MoviesAndShows.json')
-    export_to_json('Weeks', 'Weeks.json')
-    export_to_json('Countries', 'Countries.json')
-    export_to_json('Ratings', 'Ratings.json')
-    export_to_json('most_popular', 'most_popular.json')
+            # Експорт даних з кожної таблиці
+            export_to_csv('MoviesAndShows', 'MoviesAndShows.csv')
+            export_to_csv('Weeks', 'Weeks.csv')
+            export_to_csv('Countries', 'Countries.csv')
+            export_to_csv('Ratings', 'Ratings.csv')
+            export_to_csv('most_popular', 'most_popular.csv')
 
 except psycopg2.OperationalError as e:
     print(f"Unable to connect to the database. Error: {e}")
-
-finally:
-    # Закриття підключення до PostgreSQL бази даних
-    if 'conn' in locals() and conn is not None:
-        conn.close()
