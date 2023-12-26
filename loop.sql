9b4eceedc55e6af089ec394c0e03a5223d59b8f0
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 14:49:09 +0200

    Add files via upload

---
+++ loop.sql
@@ -0,0 +1,30 @@
+
+DO $$ 
+DECLARE
+    counter INT := 1;
+BEGIN
+    -- Починаємо цикл
+    LOOP
+        -- Вставка тестових даних в таблицю MoviesAndShows
+        INSERT INTO MoviesAndShows (MovieOrShowID, Title, Type, ReleaseYear)
+        VALUES (counter, 'Фільм/шоу ' || counter, 'Тип ' || counter, 2000 + counter);
+
+        -- Вставка тестових даних в таблицю Weeks
+        INSERT INTO Weeks (WeekID, WeekStartDate, WeekEndDate)
+        VALUES (counter, CURRENT_DATE + counter, CURRENT_DATE + counter + 6);
+
+        -- Вставка тестових даних в таблицю Countries
+        INSERT INTO Countries (CountryID, CountryName)
+        VALUES (counter, 'Країна ' || counter);
+
+        -- Вставка тестових даних в таблицю Ratings
+        INSERT INTO Ratings (RatingID, MovieOrShowID, WeekID, CountryID, Rank, Viewership, Duration)
+        VALUES (counter, counter, counter, counter, counter, 100000 + counter * 10000, 120 + counter);
+
+        -- Збільшуємо лічильник
+        counter := counter + 1;
+
+        -- Перевіряємо, чи потрібно завершити цикл
+        EXIT WHEN counter > 10; -- Замініть 10 на кількість рядків, яку ви хочете вставити
+    END LOOP;
+END $$;
